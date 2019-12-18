ecfb9ffa7295217492976562f86284a01ad35de2
qiaolei <a346750236@vip.qq.com>
2019-12-18 17:40:20 +0800

    登录搭建小完成

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 
 import ElementUI from 'element-ui'
-// 引入UI
 import 'element-ui/lib/theme-chalk/index.css'
+import '../src/styles/index.less'
+// 引入UI
 
 Vue.use(ElementUI) // 引入样式
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>登录页面</div>
+  <div class="login">
+    <el-card class="login-card">
+       <div class="title">
+          <img src="../../assets/img/logo_index.png" alt="">
+       </div>
+       <!-- 在 Form 组件中，每一个表单域由一个 Form-Item 组件构成，表单域中可以放置各种类型的表单控件
+       ，包括 Input、Select、Checkbox、Radio、Switch、DatePicker、TimePicker -->
+       <!-- !-- 表单 => el-form包裹 -->
+       <el-form>
+         <!-- 里面要写el-from-item -->
+         <!-- 每一个表单域由一个 Form-Item 组件构成 -->
+         <el-from-item>
+           <!-- 手机号 -->
+           <el-input  style="margin-top:20px"></el-input>
+         </el-from-item>
+         <el-from-item>
+           <!-- 验证码 -->
+           <el-input style="width:70%;margin-top:20px"></el-input>
+           <!-- 发送验证码 -->
+           <el-button style="float:right;margin-top:20px" >发送验证码</el-button>
+         </el-from-item>
+         <!-- 同意选项 -->
+         <el-from-item>
+           <el-checkbox style="margin-top:20px;font-size:30px">我已阅读并同意用户协议和隐私条款</el-checkbox>
+         </el-from-item>
+         <!-- 登录按钮 -->
+         <el-from-item>
+           <!-- 注册登录按钮 -->
+            <el-button type="primary" style="width:100%;margin-top:30px">登录</el-button>
+         </el-from-item>
+       </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,6 +40,23 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style lang="less" scoped>
+  .login{
+    background-image: url('../../assets/img/login_bg.jpg');
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card{
+      width: 480px;
+      height: 360px;
+      .title{
+        text-align: center;
+        img{
+          height: 45px;
+        }
+      }
+    }
+  }
+</style>>
