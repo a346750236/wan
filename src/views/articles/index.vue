9d387cca1061d33eb6cec89f28aed8b7f9b023ad
qiaolei <a346750236@vip.qq.com>
2019-12-24 21:45:29 +0800

    内容列表分页和三个状态切换获取信息

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -46,6 +46,7 @@
         <el-date-picker
          @change="changeCondition"
           v-model="formData.dateRange"
+          value-format="yyyy-MM-dd"
           type="daterange"
           range-separator="-"
           start-placeholder="开始日期"
@@ -55,7 +56,7 @@
     </el-row>
     <!-- 下面主体 -->
     <el-row class="total">
-      <span>共找到1000条符合条件的内容</span>
+      <span>共找到{{ page.total }}条符合条件的内容</span>
     </el-row>
     <!-- 循环的模板 -->
     <el-row
@@ -90,6 +91,18 @@
         </el-row>
       </el-col>
     </el-row>
+    <!-- 分页组件 -->
+    <el-row type='flex' justify="center" align="middle" style='height:60px'>
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="page.total"
+        :current-page="page.currentPage"
+        :page-size="page.pageSize"
+        @current-change="changePage"
+        >
+        </el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -104,7 +117,12 @@ export default {
       },
       channels: [], // 定义一个数组接收频道列表的数据
       Wzlist: [], // 定义一个数组接收文章列表的数据
-      defaultImg: require('../../assets/img/404.png')
+      defaultImg: require('../../assets/img/404.png'),
+      page: {
+        currentPage: 1, // 当前页码
+        pageSize: 10, // 文章列表最低10条
+        total: 0
+      }
     }
   },
   filters: {
@@ -142,11 +160,27 @@ export default {
     }
   },
   methods: {
-    //   总事件
+    //   改变页码事件
+    changePage (newPage) {
+      this.page.currentPage = newPage // 赋值当前页
+      // 最新状态
+      this.getConditionArticle()
+    },
+    // 改变条件
     changeCondition () {
+      this.page.currentPage = 1 // 强制将当前的页码回到第一页
+      // 最新状态
+      this.getConditionArticle()
+    },
+    // 定义一个导航切换和分页切换数据
+    getConditionArticle () {
       let params = {
+        page: this.page.currentPage, // 分页信息
+        per_page: this.page.pageSize, // 分页信息
         status: this.formData.status === 5 ? null : this.formData.status, // 不传为全部 5代表全部
-        channel_id: this.formData.channel_id // 频道id
+        channel_id: this.formData.channel_id, // 频道
+        begin_pubdate: this.formData.dateRange.length ? this.formData.dateRange[0] : null, // 起始时间
+        end_pubdate: this.formData.dateRange.length > 1 ? this.formData.dateRange[1] : null // 截止时间
       }
       // 文章列表
       this.getArticles(params)
@@ -165,7 +199,8 @@ export default {
         url: '/articles',
         params // 传就有，不传就为undefined
       }).then(result => {
-        this.Wzlist = result.data.results
+        this.Wzlist = result.data.results // 接收文章列表数据
+        this.page.total = result.data.total_count // 文章总数
       })
     }
   },
