f6b45e5b2f025933eb3047c579360e272899d3d2
qiaolei <a346750236@vip.qq.com>
2019-12-27 19:22:03 +0800

    发布文章-封面图片选择组件上传

--- src/components/publish/S-image.vue
+++ src/components/publish/S-image.vue
@@ -18,7 +18,12 @@
          ></el-pagination>
       </el-row>
     </el-tab-pane>
-    <el-tab-pane label="上传那图片" name="upload"></el-tab-pane>
+    <el-tab-pane label="上传图片" name="upload">
+          <!-- 放置一个上传组件 -->
+        <el-upload class='upload' :http-request="uploadImg" action="" :show-file-list="false">
+            <i class='el-icon-plus'></i>
+        </el-upload>
+    </el-tab-pane>
   </el-tabs>
 </template>
 
@@ -36,7 +41,19 @@ export default {
     }
   },
   methods: {
-    //   传值
+    //   上传图片
+    uploadImg (params) {
+      let data = new FormData() // 实例化对象
+      data.append('image', params.file)
+      this.$axios({
+        url: '/user/images',
+        method: 'post',
+        data
+      }).then((result) => {
+        this.$emit('SelectImg', result.data.url)
+      })
+    },
+    //   传值给C-image
     clickImg (url) {
       this.$emit('SelectImg', url)
     },
@@ -80,5 +97,17 @@ export default {
       height: 100%;
     }
   }
+}
+.upload {
+   display: flex;
+   justify-content: center;
+    i {
+        font-size: 50px;
+        display: block;
+        margin: 20px auto;
+        padding: 60px;
+        border: 1px dashed #ccc;
+        border-radius: 4px;
+    }
 }
 </style>
